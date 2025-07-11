fb9e680e81302771de0adee2e508a6204c784f5c
David Smith <smitdave@gmail.com>
2025-07-11 08:11:44 -0700

    added functionality for null function classes

--- R/xde_cohort.R
+++ R/xde_cohort.R
@@ -10,11 +10,15 @@ set_eir  = function(eir, pars){
 }
 
 #' @title Reset the mean daily EIR
-#' @description For cohort models, reset the EIR
-#' @param eir the mean eir
-#' @param pars an **`xds`** object
+#' 
+#' @description For cohort dynamcis models, set the mean EIR
+#' 
+#' @inheritParams set_eir
+#' 
 #' @return an **`xds`** object
+#' 
 #' @export
+#' 
 set_eir.cohort  = function(eir, pars){
 
   pars$EIRpar$eir <- eir  
@@ -28,6 +32,31 @@ set_eir.cohort  = function(eir, pars){
   pars$F_eir <- with(pars$EIRpar,{
      function(age, bday){
       eir/scale*F_season(age+bday)*F_trend(age+bday)*F_age(age)}
+  })
+  return(pars)
+}
+
+#' @title Reset the mean daily EIR
+#' 
+#' @description For cohort models, reset the EIR
+#' 
+#' @inheritParams set_eir
+#' 
+#' @return an **`xds`** object
+#' 
+#' @export
+#' 
+set_eir.eir = function(eir, pars){
+  
+  pars$EIRpar$eir <- eir  
+  if(length(pars$EIRpar$season_par)>0)  
+    pars$EIRpar$F_season <- make_function(pars$EIRpar$season_par)
+  if(length(pars$EIRpar$trend_par)>0)  
+    pars$EIRpar$F_trend <- make_function(pars$EIRpar$trend_par)
+  
+  pars$F_eir <- with(pars$EIRpar,{
+    function(t){
+      eir/scale*F_season(t)*F_trend(t)}
   })
   
   return(pars)
