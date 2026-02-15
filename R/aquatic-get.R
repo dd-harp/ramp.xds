03bce57fbcc0d18ce35ba2a4005a3ef14037d2cb
David L Smith <smitdave@gmail.com>
2026-02-15 11:00:12 -0800

    get_L_orbits

--- R/aquatic-get.R
+++ R/aquatic-get.R
@@ -12,7 +12,7 @@
 #' @return the orbits for the **L** component 
 #' 
 #' @export
-get_L_out = function(xds_obj, s=1){
+get_L_orbits = function(xds_obj, s=1){
   
   got = xds_obj$outputs$orbits$L[[s]]
   got$time = xds_obj$outputs$time
