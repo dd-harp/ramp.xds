a859452c51b3fce534df2537f01b4d1931ec19bd
David L Smith <smitdave@gmail.com>
2026-09-08 10:05:18 -0700

    minor change
    The function `get_variables` was moved into `get_variables.R`

---
+++ R/get_variables.R
@@ -0,0 +1,33 @@
+
+#' Get Variables
+#' 
+#' @description
+#' A generic function to retrieve variables from the **`xds`** model object
+#'
+#' @param opts an options list and dispatching
+#' @param t current simulation time
+#' @param y state vector
+#' @param xds_obj an **`xds`** model object
+#' @param ix the species index
+#'
+#' @returns a list of named variables
+#' @keywords internal
+#' @export
+get_variables = function(opts, t, y, xds_obj, ix){
+  UseMethod("get_variables", opts) 
+}
+
+#' Get Variables
+#' 
+#' @description
+#' The NULL case for [get_variables]
+#' 
+#'
+#' @inheritParams get_variables
+#' 
+#' @returns an empty list 
+#' @keywords internal
+#' @export
+get_variables.list = function(opts, t, y, xds_obj, ix){
+  list() 
+}
